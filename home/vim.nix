e8cc850336cda70820e5751ade1a23ab8455aa07
William Fligor <williamfligor@users.noreply.github.com>
2021-01-02 15:19:41 -0500

    Clean up vim as only neovim should be used

--- home/vim.nix
+++ home/vim.nix
@@ -37,18 +37,6 @@ let
         dependencies = [ python3Packages.doq ];
     };
 
-    vim-spacemacs = buildVimPlugin {
-        name = "vim-spacemacs";
-        pname = "vim-spacemacs";
-        src = pkgs.fetchFromGitHub {
-            owner = "jimmay5469";
-            repo = "vim-spacemacs";
-            rev = "a87473fd6c126f3ee85b32b6ee0d396e2093652e";
-            sha256 = "0izbjq6qbia013vmd84rdwjmwagln948jh9labhly0asnhqyrkb8";
-        };
-        dependencies = [];
-    };
-
     vim-rainbow-parentheses = buildVimPlugin {
         name = "vim-rainbow-parentheses";
         pname = "vim-rainbow-parentheses";
@@ -125,8 +113,19 @@ in
             vim-leader-guide
             fzfWrapper
             fzf-vim
-            vim-rainbow-parentheses
-            plugins.editorconfig-vim
+            {
+                plugin = vim-rainbow-parentheses;
+                config = ''
+                    " Rainbow always on
+                    autocmd FileType * RainbowParentheses
+                '';
+                }
+            {
+                plugin = plugins.editorconfig-vim;
+                config = ''
+                    let g:EditorConfig_exclude_patterns = ['fugitive://.*', 'scp://.*']
+                '';
+            }
             vim-tmux-navigator
             supertab
 
@@ -135,39 +134,148 @@ in
             vim-nix
             vim-cmake
             vim-bitbake
-            vim-markdown
+            {
+                plugin = vim-markdown;
+                config = '';
+                    let g:vim_markdown_folding_disabled = 1
+                '';
+            }
             vim-nix
         ];
 
         extraConfig = ''
+            let mapleader = "\<SPACE>"
+            let g:spacemacs#leader = '<SPACE>'
+
+            let g:spacemacs#excludes = [
+              \ 'b',
+              \ 'p',
+            \ ]
+
+            " ----- KEY MAPPINGS -------
+            nnoremap <silent> <LEADER> :<C-U>LeaderGuide '<SPACE>'<CR>
+            vnoremap <silent> <LEADER> :<C-U>LeaderGuideVisual '<SPACE>'<CR>
+            nnoremap <leader>rn :call NumberToggle()<Cr>
+            nnoremap <leader><space> :noh<cr>
+            nnoremap <space>bb :Buffers<CR>
+            nnoremap <space>pf :GFiles<CR>
+            nnoremap <space>ff :Files<CR>
+            nnoremap <space>md :Dox<CR>
+
+            " Remove trailing whitespace
+            nnoremap <leader>W :%s/\s\+$//<cr>:let @/=\'\'<CR>
+
+            " Make %% in command line expand to the path of the current buffer
+            cnoremap <expr> %% getcmdtype() == ':' ? expand('%:h').'/' : '%%'
+
+            " Reselect after indent outdent
+            vnoremap < <gv
+            vnoremap > >gv
+
+            " Reselect after indent outdent
+            vnoremap < <gv
+
+            " Built in autocomplete
+            set completeopt+=menuone 
+            set completeopt+=noselect
+            set completeopt+=noinsert
+            set shortmess+=c   " Shut off completion messages
+            set belloff+=ctrlg " If Vim beeps during completion
+
+            " Searching
+            set incsearch
+            set showmatch
+            set hlsearch
+
+            set tabstop=4
+            set shiftwidth=4
+            set softtabstop=4
+            set expandtab
+            set showcmd
+            set showmode
+            set visualbell
+            set cursorline
+            set ttyfast
+            set ruler
+            set laststatus=2
+            set nrformats=
+            set number
+            set undofile
+            set relativenumber
+
+            " Theming
+            syntax on
+            set background=dark
+
+            set termguicolors
+            let base16colorspace=256
+            silent! colorscheme base16-tomorrow-night
+
+            " Airline Theme
+            let g:airline_theme='tomorrow'
+            let g:airline_powerline_fonts = 1
+
+            " Restore cursor position to where it was before
+            augroup JumpCursorOnEdit
+                au!
+                autocmd BufReadPost *
+                            \ if expand("<afile>:p:h") !=? $TEMP |
+                            \   if line("'\"") > 1 && line("'\"") <= line("$") |
+                            \     let JumpCursorOnEdit_foo = line("'\"") |
+                            \     let b:doopenfold = 1 |
+                            \     if (foldlevel(JumpCursorOnEdit_foo) > foldlevel(JumpCursorOnEdit_foo - 1)) |
+                            \        let JumpCursorOnEdit_foo = JumpCursorOnEdit_foo - 1 |
+                            \        let b:doopenfold = 2 |
+                            \     endif |
+                            \     exe JumpCursorOnEdit_foo |
+                            \   endif |
+                            \ endif
+                " Need to postpone using "zv" until after reading the modelines.
+                autocmd BufWinEnter *
+                            \ if exists("b:doopenfold") |
+                            \   exe "normal zv" |
+                            \   if(b:doopenfold > 1) |
+                            \       exe  "+".1 |
+                            \   endif |
+                            \   unlet b:doopenfold |
+                            \ endif
+            augroup END
+
+            " Toggle relative numbers
+            function! NumberToggle()
+                if(&relativenumber == 1)
+                    set norelativenumber
+                    set number
+                else
+                    set nonumber
+                    set relativenumber
+                endif
+            endfunc
+
+            let swapdir = expand('~/.vim/swap')
+            if !isdirectory(swapdir)
+                call mkdir(swapdir)
+            endif
+
+            let undodir = expand('~/.vim/undo')
+            if !isdirectory(undodir)
+                call mkdir(undodir)
+            endif
+
+            let backupdir = expand('~/.vim/backup')
+            if !isdirectory(backupdir)
+                call mkdir(backupdir)
+            endif
+
+            set undodir=~/.vim/undo//
+            set backupdir=~/.vim/backup//
+            set directory=~/.vim/swap//
+
             ${readSettingsFile "spacemacs.vim"}
-            ${readSettingsFile "autocomplete.vim"}
             ${readSettingsFile "indent-tcl.vim"}
-            ${readSettingsFile "keys.vim"}
-            ${readSettingsFile "opts.vim"}
-            ${readSettingsFile "theme.vim"}
-            ${readSettingsFile "utils.vim"}
 
             call SpacemacsInit()
         '';
 
     };
-
-    programs.vim = {
-        enable = true;
-
-        plugins = config.programs.neovim.plugins;
-        extraConfig = ''
-            ${readSettingsFile "spacemacs.vim"}
-            ${readSettingsFile "autocomplete.vim"}
-            ${readSettingsFile "indent-tcl.vim"}
-            ${readSettingsFile "keys.vim"}
-            ${readSettingsFile "opts.vim"}
-            ${readSettingsFile "theme.vim"}
-            ${readSettingsFile "utils.vim"}
-
-            call SpacemacsInit()
-        '';
-    };
-
 }
